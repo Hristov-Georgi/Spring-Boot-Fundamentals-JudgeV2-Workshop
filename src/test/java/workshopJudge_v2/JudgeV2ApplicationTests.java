0faaf0fcb74f68c409b8009236985a73aa74b150
Georgi <hristovgeorgivalentinov@gmail.com>
2024-03-29 09:38:58 +0200

    initial commit

---
+++ src/test/java/workshopJudge_v2/JudgeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package workshopJudge_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudgeV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
